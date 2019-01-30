c389c5d777ff74dd5ad8985239c6cc3fd0d11231
timtan93 <timtan93@gmail.com>
2019-01-30 11:36:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('div.target')
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n){
+  const lis = document
+  .querySelectorAll('#app ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++) {
+  let num = parseInt(lis[i].innerHTML);
+  lis[i].innerHTML = (num + n).toString();
+  }
+}
